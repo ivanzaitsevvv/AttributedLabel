c098644fa798f4137d96fe5516e282d9e96803b1
Ivan Zaitsev <ivan.zaitsev1992@gmail.com>
2017-10-05 23:07:54 +0300

    Project created

---
+++ AttributedLabel/AttributedLabel.swift
@@ -0,0 +1,13 @@
+//
+//  AttributedLabel.swift
+//  AttributedLabelExample
+//
+//  Created by Ivan Zaitsev on 10/5/17.
+//  Copyright © 2017 Ivan Zaitsev. All rights reserved.
+//
+
+import UIKit
+
+class AttributedLabel: UILabel {
+
+}
